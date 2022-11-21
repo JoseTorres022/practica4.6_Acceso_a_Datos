9c2cd4a1af844d14ff8f277726597b88e83a5384
JoseTorres022 <joseangel.tosa@outlook.com>
2022-11-21 14:13:38 -0700

    creacio de un crud php, mysql con xampp

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Practica 4.6 - Acceso a Datos</title>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
+</head>
+<body>
+    <h1>Las liebres del ITCJ Campus 1</h1>
+    <!-- <p>CRUD, aplicación experimental</p> -->
+</body>
+</html>
